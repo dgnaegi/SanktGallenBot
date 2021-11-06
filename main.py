94c9c4fcd572809e7ba33b4de797f02cc798db76
Daniel Gnaegi <mail@dgnaegi.ch>
2021-11-06 20:58:39 +0100

    improve conversation context handling

--- main.py
+++ main.py
@@ -9,26 +9,31 @@ from collectionpoint.handlers import collectionPointDefault, collectionPointLoca
 from carpark.handlers import carparkDefault, carparkLocation
 from common.expectations import Expectations
 from disposal.handlers import disposalArea, disposalDefault, disposalStop
-    
+
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Willkommen, ich bin der SanktGallenBot und helfe dir gerne weiter 😘")
     update.message.reply_text("Unten links im Menü findest du meine Befehle")
     update.message.reply_text("Für weitere Infos klicke hier: /help")
-    
+
 def help(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Dieser Bot wurde inital im Rahmen des Open Data Hack St.Gallen 2021 erstellt. Er hilft dir öffentliche Daten zu beziehen.")
     update.message.reply_text("Die möglichen Befehle siehst du im Menu unten links.")
     update.message.reply_text("Weitere Informationen findest du auf der Projekthomepage https://sanktgallenbot.ch oder kontaktiere @ignobled")
 
-def main() -> None:    
+def unexcpected(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Das war unerwartet 😲")
+    update.message.reply_text("Bitte sende mir nochmals einen Befehl")
+    return ConversationHandler.END
+
+def main() -> None:
     with open('config.json') as data_file:
             data = json.load(data_file)
             token = data["token"]
-    
+
     updater = Updater(token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-        
+
     _handlers = {}
     _handlers['start_handler'] = CommandHandler('start', start)
     _handlers['help_handler'] = CommandHandler('help', help)
@@ -36,37 +41,37 @@ def main() -> None:
     _handlers['carpark_conversation_handler'] = ConversationHandler(
         entry_points=[CommandHandler('parkhaus', carparkDefault)],
         states={
-            Expectations.Location: [MessageHandler(Filters.location, carparkLocation), [CommandHandler('parkhaus', carparkDefault)]],
+            Expectations.Location: [MessageHandler(Filters.location, carparkLocation)],
         },
-        fallbacks=[CommandHandler('error', error)]
+        fallbacks=[MessageHandler(Filters.command, unexcpected)]
     )
     _handlers['collectionpoint_conversation_handler'] = ConversationHandler(
         entry_points=[CommandHandler('sammelstelle', collectionPointDefault)],
         states={
-            Expectations.Location: [MessageHandler(Filters.location, collectionPointLocation), [CommandHandler('sammelstelle', collectionPointDefault)]],
+            Expectations.Location: [MessageHandler(Filters.location, collectionPointLocation)],
         },
-        fallbacks=[CommandHandler('error', error)]
+        fallbacks=[MessageHandler(Filters.command, unexcpected)]
     )
     _handlers['chargingstation_conversation_handler'] = ConversationHandler(
         entry_points=[CommandHandler('ladestation', chargingStationDefault)],
         states={
-            Expectations.Location: [MessageHandler(Filters.location, chargingStationLocation), CommandHandler('ladestation', chargingStationDefault)],
+            Expectations.Location: [MessageHandler(Filters.location, chargingStationLocation)],
         },
-        fallbacks=[CommandHandler('error', error)]
+        fallbacks=[MessageHandler(Filters.command, unexcpected)]
     )
     _handlers['disposal_conversation_handler'] = ConversationHandler(
         entry_points=[CommandHandler('abfuhr', disposalDefault)],
         states={
-            Expectations.Text: [CallbackQueryHandler(disposalArea), CommandHandler('abfuhr', disposalDefault)],
+            Expectations.Text: [CallbackQueryHandler(disposalArea)],
         },
-        fallbacks=[CommandHandler('error', error)]
+        fallbacks=[MessageHandler(Filters.command, unexcpected)]
     )
-    
+
     for name, _handler in _handlers.items():
         print(f'Adding handler {name}')
         dispatcher.add_handler(_handler)
-    
-    
+
+
     updater.start_polling()
     updater.idle()
 
